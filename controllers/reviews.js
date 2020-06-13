8bdd9186afaf1fe36b738e04e7d3194e410f3059
johnrobertmahlan <johnrobertmahlan@gmail.com>
2020-06-13 12:08:26 -0600

    implement edit functionality

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -3,13 +3,15 @@ const Bookstore = require('../models/bookstore');
 module.exports = {
     create: userReview,
     delete: deleteReview,
-    show
+    show,
+    edit: editReview
 }
 
 function userReview(req, res) {
     Bookstore.findById(req.params.id, function(err, bookstore) {
         req.body.createdBy = req.user._id;
         bookstore.reviews.push(req.body);
+        console.log(req.body);
         bookstore.save(function(err) {
             res.redirect('/bookstores');
         });
@@ -27,8 +29,18 @@ function deleteReview(req, res) {
 
 function show(req, res) {
     Bookstore.find({"reviews._id": req.params.id},function(err, bookstore) {
-        console.log(req.params.id);
-        console.log(bookstore);
         res.render('reviews/show', {user: req.user, bookstore});
+    });
+};
+
+function editReview(req, res) {
+    Bookstore.find({"reviews._id": req.params.id, "reviews.createdBy": req.user._id}, function(err, bookstore) {
+        let review = bookstore[0].reviews.id(req.params.id);
+        review.content = req.body.content;
+        review.rating = req.body.rating;
+        bookstore[0].save(function(err) {
+            console.log(err);
+            res.redirect('/bookstores');
+        });
     });
 };
--- routes/reviews.js
+++ routes/reviews.js
@@ -5,7 +5,7 @@ const reviewsCtrl = require('../controllers/reviews');
 router.post('/bookstores/:id/reviews', reviewsCtrl.create);
 
 router.get('/reviews/:id', reviewsCtrl.show);
-
+router.put('/reviews/:id', reviewsCtrl.edit);
 router.delete('/reviews/:id', reviewsCtrl.delete);
 
 module.exports = router;

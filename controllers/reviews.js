bbad9595951f7734437dc2597f04df10b2649f10
johnrobertmahlan <johnrobertmahlan@gmail.com>
2020-06-13 10:01:32 -0600

    add edit review button and show review functionality

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -2,7 +2,8 @@ const Bookstore = require('../models/bookstore');
 
 module.exports = {
     create: userReview,
-    delete: deleteReview
+    delete: deleteReview,
+    show
 }
 
 function userReview(req, res) {
@@ -21,5 +22,13 @@ function deleteReview(req, res) {
         bookstore[0].save(function(err) {
             res.redirect('/bookstores')
         });
+    });
+};
+
+function show(req, res) {
+    Bookstore.find({"reviews._id": req.params.id},function(err, bookstore) {
+        console.log(req.params.id);
+        console.log(bookstore);
+        res.render('reviews/show', {user: req.user, bookstore});
     });
 };
--- routes/reviews.js
+++ routes/reviews.js
@@ -4,6 +4,8 @@ const reviewsCtrl = require('../controllers/reviews');
 
 router.post('/bookstores/:id/reviews', reviewsCtrl.create);
 
+router.get('/reviews/:id', reviewsCtrl.show);
+
 router.delete('/reviews/:id', reviewsCtrl.delete);
 
 module.exports = router;
